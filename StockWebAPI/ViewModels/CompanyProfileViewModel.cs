6647182001133e98cd075a58b375e4f3dcf6e6ea
Anthony Southavilay <anthony.southavilay@gmail.com>
2020-12-20 18:15:08 -0500

    Created CompanyProfileViewModel and Address class

---
+++ StockWebAPI/ViewModels/CompanyProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using StockWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockWebAPI.ViewModels
+{
+    public class CompanyProfileViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CEO { get; set; }
+        public Address Address { get; set; }
+        public string WebsiteUrl { get; set; }
+        public int NumberOfEmployees { get; set; }
+        public string Sector { get; set; }
+        public string Industry { get; set; }
+        public string Exchange { get; set; }
+    }
+}
