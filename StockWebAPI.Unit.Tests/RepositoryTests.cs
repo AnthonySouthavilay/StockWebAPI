1975baeb0459ec3ea4bfe3c9515fc4bf15a859da
Anthony Southavilay <anthony.southavilay@gmail.com>
2020-12-20 15:58:15 -0500

    Created IEXRepo w/ tests

---
+++ StockWebAPI.Unit.Tests/RepositoryTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using StockWebAPI.Models;
+using StockWebAPI.Repository;
+using FluentAssertions;
+using System;
+
+namespace StockWebAPI.Unit.Tests
+{
+    public class RepositoryTests
+    {
+        private IEXRepository iexRepo = new IEXRepository();
+        private readonly string testStockSymbol = "TANK";
+
+        [Test]
+        public void GetCompanyInformation_ValidSymbol_ReturnsModel()
+        {
+            var result = iexRepo.GetCompanyInfo(testStockSymbol);
+            result.Should().NotBeNull();
+        }
+
+        [TestCase("T@NK")]
+        [TestCase("T@N3K")]
+        [TestCase("@!NK")]
+        public void GetCompanyInformation_InvalidSymbol_ReturnsArgumentException(string invalidSymbol)
+        {
+            var company = new CompanyInfo();
+            Action act = () => iexRepo.GetCompanyInfo(invalidSymbol);
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ StockWebAPI/Repository/IEXRepository.cs
@@ -0,0 +1,30 @@
+﻿using StockWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StockWebAPI.Repository
+{
+    public class IEXRepository
+    {
+        public CompanyInfo GetCompanyInfo(string stockSymbol)
+        {
+            if (ContainsSpecialCharacter(stockSymbol))
+            {
+                throw new ArgumentException($"{stockSymbol} is not a valid stock symbol");
+            }
+            return new CompanyInfo()
+            {
+                symbol = stockSymbol
+            };
+        }
+
+        private static bool ContainsSpecialCharacter(string input)
+        {
+            Regex rgx = new Regex("[^A-Za-z0-9]");
+            return rgx.IsMatch(input);
+        }
+    }
+}
