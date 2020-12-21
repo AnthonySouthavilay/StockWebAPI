37b7d9ca7f5a5ac930aabf7f28ae339833dd3fae
Ant South <anthony.southavilay@gmail.com>
2020-12-21 08:48:54 -0500

    Created TestHelpers/MockHandlers

---
+++ StockWebAPI.Unit.Tests/TestHelpers/MockExceptionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StockWebAPI.Unit.Tests.TestHelpers
+{
+    public class MockExceptionHandler : HttpMessageHandler
+    {
+        private Exception mockException;
+        public MockExceptionHandler(Exception exception)
+        {
+            this.mockException = exception;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            throw mockException;
+        }
+    }
+}
