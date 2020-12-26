6e8c11bb7eedb5d278f31ae104bdd6a964d5ec49
Tank Southy <tank.southy@gmail.com>
2020-12-26 00:45:41 -0500

    created AlphaVantageQuote

---
+++ StockWebAPI/Models/AlphaVantage/AlphaVantageQuote.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace StockWebAPI.Models.AlphaVantage
+{
+    public class GlobalQuote
+    {
+        private string changePercent;
+
+        [JsonProperty("01.symbol")]
+        public string Symbol { get; set; }
+        [JsonProperty("02.open")]
+        public string Open { get; set; }
+        [JsonProperty("03.high")]
+        public string High { get; set; }
+        [JsonProperty("04.low")]
+        public string Low { get; set; }
+        [JsonProperty("05.price")]
+        public string Price { get; set; }
+        [JsonProperty("06.volume")]
+        public string Volume { get; set; }
+        [JsonProperty("07.latesttradingday")]
+        public string LatestTradingDay { get; set; }
+        [JsonProperty("08.previousclose")]
+        public string PreviousClose { get; set; }
+        [JsonProperty("09.change")]
+        public string Change { get; set; }
+        [JsonProperty("10.changepercent")]
+        public string ChangePercent 
+        { 
+            get { return changePercent; } 
+            set { changePercent = value.Substring(0,value.Length - 1); } 
+        } 
+    }
+
+    public class AlphaVantageQuote
+    {
+        public GlobalQuote GlobalQuote { get; set; }
+    }
+}
--- StockWebAPI/ViewModels/CompanySummaryViewModel.cs
+++ StockWebAPI/ViewModels/CompanySummaryViewModel.cs
@@ -33,31 +33,31 @@ namespace StockWebAPI.ViewModels
         public string EPS { get; set; }
         public double? DividendYield { get; set; }  
 
-        public CompanySummaryViewModel ConvertToCompanySummaryViewModel(Quote quote)
+        public CompanySummaryViewModel ConvertToCompanySummaryViewModel(IEXQuote quote)
         {
             return new CompanySummaryViewModel()
             {
                 CompanyName = quote.companyName,
                 Symbol = quote.symbol,
                 Exchange = quote.primaryExchange,
-                PreviousClose = (double?)quote.previousClose,
-                OpenPrice = (double?)quote.open,
-                Price = (double?)quote.latestPrice,
-                Bid = (double?)quote.iexBidPrice,
-                Ask = (double?)quote.iexAskPrice,
-                HighPrice = (double?)quote.high,
-                LowPrice = (double?)quote.low,
-                PriceChange = (double?)quote.change,
-                PriceChangePercent = (double?)quote.changePercent,
+                PreviousClose = quote.previousClose,
+                OpenPrice = quote.open,
+                Price = quote.latestPrice,
+                Bid = quote.iexBidPrice,
+                Ask = quote.iexAskPrice,
+                HighPrice = quote.high,
+                LowPrice = quote.low,
+                PriceChange = quote.change,
+                PriceChangePercent = quote.changePercent,
                 Week52High = quote.week52High.ToString(),
                 Week52Low = quote.week52Low.ToString(),
-                Volume = (int?)quote.volume,
-                AverageVolume = (int?)quote.avgTotalVolume,
+                Volume = quote.volume,
+                AverageVolume = quote.avgTotalVolume,
                 MarketCap = quote.marketCap.ToString(),
                 PERatio = quote.peRatio.ToString()
             };
         }
-        public CompanySummaryViewModel ConvertToCompanySummaryViewModel(CompanyKeyStats keyStats)
+        public CompanySummaryViewModel ConvertToCompanySummaryViewModel(CompanyKeyStats keyStats, AlphaVantageQuote alphaVantageQuote)
         {
             return new CompanySummaryViewModel()
             {
@@ -68,7 +68,15 @@ namespace StockWebAPI.ViewModels
                 Week52Low = keyStats._52WeekLow,
                 MarketCap = keyStats.MarketCapitalization,
                 PERatio = keyStats.PERatio,
-                EPS = keyStats.EPS
+                EPS = keyStats.EPS,
+                OpenPrice = double.Parse(alphaVantageQuote.GlobalQuote.Open),
+                Price = double.Parse(alphaVantageQuote.GlobalQuote.Price),
+                HighPrice = double.Parse(alphaVantageQuote.GlobalQuote.High),
+                LowPrice = double.Parse(alphaVantageQuote.GlobalQuote.Low),
+                Volume = int.Parse(alphaVantageQuote.GlobalQuote.Volume),
+                PreviousClose = double.Parse(alphaVantageQuote.GlobalQuote.PreviousClose),
+                PriceChange = double.Parse(alphaVantageQuote.GlobalQuote.Change),
+                PriceChangePercent = double.Parse(alphaVantageQuote.GlobalQuote.ChangePercent),
             };
         }
     }
