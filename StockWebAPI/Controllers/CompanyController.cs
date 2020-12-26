3d9d95be68e8ae72b3bb53687d9728d03bd51b7f
Tank Southy <tank.southy@gmail.com>
2020-12-26 17:55:17 -0500

    created CompanySummary API endpoint

---
+++ StockWebAPI/Controllers/CompanyController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StockWebAPI.Service;
+using StockWebAPI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace StockWebAPI.Controllers
+{
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private CompanyService companyService;
+        private readonly HttpClient _httpClient;
+
+        public CompanyController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [HttpGet]
+        [Route("{companySymbol}/CompanySummary")]
+        public async Task<CompanySummaryViewModel> GetCompanySummaryAsync(string companySymbol)
+        {
+            companyService = new CompanyService(_httpClient);
+            return await companyService.GetCompanySummaryAsync(companySymbol);
+        }
+    }
+}
