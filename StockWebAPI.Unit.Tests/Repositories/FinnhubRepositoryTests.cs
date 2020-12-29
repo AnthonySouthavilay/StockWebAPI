6b8ca2d5934f40be532161d78623ac5f32904492
Tank Southy <tank.southy@gmail.com>
2020-12-28 21:00:01 -0500

    started repo

---
+++ StockWebAPI.Unit.Tests/Repositories/FinnhubRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using StockWebAPI.Models.Finnhub;
+using StockWebAPI.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockWebAPI.Unit.Tests.Repositories
+{
+    class FinnhubRepositoryTests
+    {
+        private FinnhubRepository finnhubRepo;
+        [Test]
+        public async Task GetCompanyNews_ValidSymbol_ReturnsCompanyNewsModelAsync()
+        {
+            string symbol = "AAPL";
+            HttpClient realHttp = new HttpClient();
+            finnhubRepo = new FinnhubRepository(realHttp);
+            CompanyNews result = await finnhubRepo.GetCompanyNewsAsync(symbol);
+            result.Should().NotBeNull();
+        }
+    }
+}
---
+++ StockWebAPI/Repository/FinnhubRepository.cs
@@ -0,0 +1,36 @@
+﻿using StockWebAPI.Models.Finnhub;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace StockWebAPI.Repository
+{
+    public class FinnhubRepository
+    {
+        private HttpClient _httpClient;
+        private string _requestUrl = "https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2020-04-30&to=2020-05-01&token=";
+        private const string _token = "bvl85tn48v6sqkppa030";
+
+        public FinnhubRepository(HttpClient httpClient)
+        {
+            this._httpClient = httpClient;
+        }
+
+        public async Task<CompanyNews> GetCompanyNewsAsync(string symbol)
+        {
+            CompanyNews companyNews;
+            try
+            {
+                companyNews = await _httpClient.GetFromJsonAsync<CompanyNews>(new Uri($"{_requestUrl}{_token}"));
+                return companyNews;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
