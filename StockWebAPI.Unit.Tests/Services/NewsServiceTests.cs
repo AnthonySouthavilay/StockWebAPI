8484768af7e108ffcdf827002d5f4dbcafd6aa5a
Tank Southy <tank.southy@gmail.com>
2021-01-01 21:42:26 -0500

    added GetCompanyNews to Service; added Extension

---
+++ StockWebAPI.Unit.Tests/Services/NewsServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using StockWebAPI.Service;
+using StockWebAPI.Unit.Tests.TestHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using StockWebAPI.ViewModels;
+using FluentAssertions;
+using StockWebAPI.Models.Finnhub;
+
+namespace StockWebAPI.Unit.Tests.Services
+{
+    public class NewsServiceTests
+    {
+        private HttpClient _httpClient;
+        private MockMessageHandler _mockMessageHandler;
+        private NewsService _sut;
+
+        [Test]
+        public async Task GetCompanyNews_ValidSymbol_ReturnsArrayOfViewModels()
+        {
+            string symbol = "TANK";
+            string mockResponse = "[{\"category\":\"company\",\"datetime\":1588377600,\"headline\":\"WhatsApp Suddenly Gets Powerful\",\"id\":691320,\"image\":\"https:www.test.com\",\"related\":\"AAPL\"," +
+                "\"source\":\"https:www.forbes.com\",\"summary\":\"WhatsApp has been seriously boosted this week\u2014and from two very unlikely sources.\",\"url\":\"https:www.forbes.comsites\"}," +
+                "{\"category\":\"company\",\"datetime\":1588377600,\"headline\":\"2020 iPhone Alert: Apple\u2019s New Price Changes Revealed\",\"id\":691319,\"image\":\"https:thumbor.forbes.comimage\"," +
+                "\"related\":\"AAPL\",\"source\":\"https:www.forbes.com\",\"summary\":\"Apple's most exciting iPhone 12 change is a shock...\",\"url\":\"https:www.forbes.comsites\"}]";
+            _mockMessageHandler = new MockMessageHandler(mockResponse, HttpStatusCode.OK);
+            _httpClient = new HttpClient(_mockMessageHandler);
+            _sut = new NewsService(_httpClient);
+            CompanyNewsViewModel[] result = await _sut.GetCompanyNewsAsync(symbol);
+            result.Should().NotBeNull();
+        }
+
+        [TestCase("T@NK")]
+        [TestCase("T@N3K")]
+        [TestCase("@!NK")]
+        [TestCase(" TANK")]
+        [TestCase("TN3K ")]
+        [TestCase(null)]
+        [TestCase("")]
+        public void GetCompanyNews_InvalidSymbol_ReturnsArgumentException(string invalidSymbol)
+        {
+            _mockMessageHandler = new MockMessageHandler("", HttpStatusCode.OK);
+            _httpClient = new HttpClient(_mockMessageHandler);
+            _sut = new NewsService(_httpClient);
+            Func<Task> result = async () => { await _sut.GetCompanyNewsAsync(invalidSymbol); };
+            result.Should().Throw<ArgumentException>();
+        }
+    }
+}
--- StockWebAPI/Service/NewsService.cs
+++ StockWebAPI/Service/NewsService.cs
@@ -1,19 +1,51 @@
-﻿using System;
+﻿using StockWebAPI.ViewModels;
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using StockWebAPI.Models.Finnhub;
+using StockWebAPI.Repository;
+using System.Collections.Generic;
+using StockWebAPI.Helpers;
+
 namespace StockWebAPI.Service
 {
     public class NewsService
     {
-        public NewsService()
+        private HttpClient _httpClient;
+        private FinnhubRepository _finnhubRepo;
+        public NewsService(HttpClient httpClient)
         {
+            this._httpClient = httpClient;
+            this._finnhubRepo = new FinnhubRepository(_httpClient);
         }
 
-        // https://mediastack.com/documentation
+        public async Task<CompanyNewsViewModel[]> GetCompanyNewsAsync(string symbol)
+        {
+            CompanyNews[] companyNews;
+            List<CompanyNewsViewModel> companyNewsViewModel = new List<CompanyNewsViewModel>();
+            if (symbol.IsValid())
+            {
+                try
+                {
+                    companyNews = await _finnhubRepo.GetCompanyNewsAsync(symbol);
+                    foreach (CompanyNews news in companyNews)
+                    {
+                        companyNewsViewModel.Add(new CompanyNewsViewModel().ConvertToCompanyNewsViewModel(news));
+                    }
+                    return companyNewsViewModel.ToArray();
+                }
+                catch
+                {
+                    throw new Exception($"There was an issue retrieving company news.");
+                }
+            }
+            throw new ArgumentException($"{symbol} is not a valid stock symbol");
+        }
 
-        // MediaStack - Live News Search; 500 calls a month
+        //Finnhub - Market News - General
 
-        // GNews - Top Headlines Search; search by sector?; 100 calls a day
+        //Finnhub - Market News - Crypto
 
-        // GNews - Search Articles for keywords Search; 100 calls a day
-
+        //
     }
 }
