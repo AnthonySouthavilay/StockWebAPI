58c99564998969be526abdd1f99b7f8b041280fa
Tank Southy <tank.southy@gmail.com>
2021-05-08 18:10:55 -0400

    added date input params; updated URI helper;

--- StockWebAPI.Unit.Tests/Repositories/GNewsRepositoryTests.cs
+++ StockWebAPI.Unit.Tests/Repositories/GNewsRepositoryTests.cs
@@ -1,14 +1,11 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
-using StockWebAPI.Models.GNews;
+using StockWebAPI.Helpers;
 using StockWebAPI.Repository;
 using StockWebAPI.Unit.Tests.TestHelpers;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace StockWebAPI.Unit.Tests.Repositories
@@ -18,16 +15,73 @@ namespace StockWebAPI.Unit.Tests.Repositories
         private GNewsRepository _gNewsRepository;
         private HttpClient _httpClient;
         private MockMessageHandler _mockMessageHandler;
+        private const string validSymbol = "GOOG";
+        private const string invalidSymbol = "G00G";
 
         [Test]
-        public async Task GetGNewsAsync_ValidSymbol_ReturnsCompanyNewsViewModelAsync()
+        public async Task GetGNewsAsync_ValidSymbol_ReturnsIEnumerableOfCompanyNewsArticles()
         {
-            string validSymbol = "GOOD";
             string mockResponse = "{\"totalArticles\":103,\"articles\":[{\"title\":\"Warren Buffett praises AAPL stock in annual letter\",\"description\":\"Warren Buffetts Berkshire Hathaway published its annual letter this morning, and it includes details on its AAPL holdings.\",\"content\":\"Warren Buffetts Berkshire Hathaway Inc. published its highly-anticipated annual letter this morning, offer details on the conglomerates investments in 2020. In this year s letter, Buffett touted that Apple ranks as Berkshire s biggest common stock ... [2562 chars]\",\"url\":\"https:  9to5mac.com 2021 02 27 warren-buffett-aapl-stake-value \",\"image\":\"https:  i2.wp.com 9to5mac.com wp-content uploads sites 6 2018 08 Warre\",\"publishedAt\":\"2021-02-27T19:19:39Z\",\"source\":{\"name\":\"9to5Mac\",\"url\":\"https:  9to5mac.com\"}},{\"title\":\"6 Key Questions Apple Answered At The Annual Shareholder Event\",\"description\":\"Shares of Apple Inc (NASDAQ:AAPL) has taken a beating in the recent market sell-off, spearheaded by tech stocks. At its annual shareholder event T...\",\"content\":\"Shares of Apple Inc (NASDAQ:AAPL) has taken a beating in the recent market sell-off, spearheaded by tech stocks. At its annual shareholder event Tuesday, the company assuaged concerns by answering a slew of questions in the Q&A sessionre are the ... [2278 chars]\",\"url\":\"https:  markets.businessinsider.com news stocks 6-key-questions-apple-answered-at-the-annual-shareholder-event-1030120836\",\"image\":\"https:  cdn.benzinga.com files imagecache 1024x768xUP images story 2012 appletv_timcook_2_2_12.png\",\"publishedAt\":\"2021-02-25T07:07:56Z\",\"source\":{\"name\":\"Business Insider\",\"url\":\"https:  markets.businessinsider.com\"}}]}";
             _mockMessageHandler = new MockMessageHandler(mockResponse, HttpStatusCode.OK);
             _httpClient = new HttpClient(_mockMessageHandler);
             _gNewsRepository = new GNewsRepository(_httpClient);
             var result = await _gNewsRepository.GetGNewsAsync(validSymbol);
+            result.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public void GetGNewsAsync_InvalidSymbol_ThrowsApiException()
+        {
+            _mockMessageHandler = new MockMessageHandler("", HttpStatusCode.NotFound);
+            _httpClient = new HttpClient(_mockMessageHandler);
+            _gNewsRepository = new GNewsRepository(_httpClient);
+            Func<Task> act = async () => await Task.Run(() => _gNewsRepository.GetGNewsAsync(invalidSymbol));
+            act.Should().Throw<ApiException>();
+        }
+
+        [Test]
+        public async Task GetGNewsAsync_ValidSymbolWithStartAndEndDate_ReturnsIEnumerableOfCompanyNewsArticles()
+        {
+            string mockResponse = "{\"totalArticles\":2,\"articles\":[{\"title\":\"Warren Buffett praises AAPL stock in annual letter\",\"description\":\"Warren Buffetts Berkshire Hathaway published its annual letter this morning, and it includes details on its AAPL holdings.\",\"content\":\"Warren Buffetts Berkshire Hathaway Inc. published its highly-anticipated annual letter this morning, offer details on the conglomerates investments in 2020. In this years letter, Buffett touted that Apple ranks as Berkshires biggest common stock ... [2562 chars]\",\"url\":\"https:  9to5mac.com 2021 02 27 warren-buffett-aapl-stake-value \",\"image\":\"https:  i2.wp.com 9to5mac.com wp-content uploads sites 6 2018 08 Warren-Buffett.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1\",\"publishedAt\":\"2021-02-27T19:19:39Z\",\"source\":{\"name\":\"9to5Mac\",\"url\":\"https:  9to5mac.com\"}},{\"title\":\"6 Key Questions Apple Answered At The Annual Shareholder Event\",\"description\":\"Shares of Apple Inc (NASDAQ:AAPL) has taken a beating in the recent market sell-off, spearheaded by tech stocks. At its annual shareholder event T...\",\"content\":\"Shares of Apple Inc (NASDAQ:AAPL) has taken a beating in the recent market sell-off, spearheaded by tech stocks. At its annual shareholder event Tuesday, the company assuaged concerns by answering a slew of questions in the Q&A session.Here are the ... [2278 chars]\",\"url\":\"https:  markets.businessinsider.com news stocks 6-key-questions-apple-answered-at-the-annual-shareholder-event-1030120836\",\"image\":\"https:  cdn.benzinga.com files imagecache 1024x768xUP images story 2012 appletv_timcook_2_2_12.png\",\"publishedAt\":\"2021-02-25T07:07:56Z\",\"source\":{\"name\":\"Business Insider\",\"url\":\"https:  markets.businessinsider.com\"}}]}";
+            string startDate = "2021-02-25";
+            string endDate = "2021-02-28";
+            _mockMessageHandler = new MockMessageHandler(mockResponse, HttpStatusCode.OK);
+            _httpClient = new HttpClient(_mockMessageHandler);
+            _gNewsRepository = new GNewsRepository(_httpClient);
+            var result = await _gNewsRepository.GetGNewsAsync(validSymbol, startDate, endDate);
+            result.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public async Task GetGNewsAsync_ValidSymbolNoDates_ReturnsIEnumerableOfCompanyNewsArticles()
+        {
+            string mockResponse = "{\"totalArticles\":2,\"articles\":[{\"title\":\"Warren Buffett praises AAPL stock in annual letter\",\"description\":\"Warren Buffetts Berkshire Hathaway published its annual letter this morning, and it includes details on its AAPL holdings.\",\"content\":\"Warren Buffetts Berkshire Hathaway Inc. published its highly-anticipated annual letter this morning, offer details on the conglomerates investments in 2020. In this years letter, Buffett touted that Apple ranks as Berkshires biggest common stock ... [2562 chars]\",\"url\":\"https:  9to5mac.com 2021 02 27 warren-buffett-aapl-stake-value \",\"image\":\"https:  i2.wp.com 9to5mac.com wp-content uploads sites 6 2018 08 Warren-Buffett.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1\",\"publishedAt\":\"2021-02-27T19:19:39Z\",\"source\":{\"name\":\"9to5Mac\",\"url\":\"https:  9to5mac.com\"}},{\"title\":\"6 Key Questions Apple Answered At The Annual Shareholder Event\",\"description\":\"Shares of Apple Inc (NASDAQ:AAPL) has taken a beating in the recent market sell-off, spearheaded by tech stocks. At its annual shareholder event T...\",\"content\":\"Shares of Apple Inc (NASDAQ:AAPL) has taken a beating in the recent market sell-off, spearheaded by tech stocks. At its annual shareholder event Tuesday, the company assuaged concerns by answering a slew of questions in the Q&A session.Here are the ... [2278 chars]\",\"url\":\"https:  markets.businessinsider.com news stocks 6-key-questions-apple-answered-at-the-annual-shareholder-event-1030120836\",\"image\":\"https:  cdn.benzinga.com files imagecache 1024x768xUP images story 2012 appletv_timcook_2_2_12.png\",\"publishedAt\":\"2021-02-25T07:07:56Z\",\"source\":{\"name\":\"Business Insider\",\"url\":\"https:  markets.businessinsider.com\"}}]}";
+            _mockMessageHandler = new MockMessageHandler(mockResponse, HttpStatusCode.OK);
+            _httpClient = new HttpClient(_mockMessageHandler);
+            _gNewsRepository = new GNewsRepository(_httpClient);
+            var result = await _gNewsRepository.GetGNewsAsync("AAPL");
+            result.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public async Task GetNewsAsync_ValidSymbolWithOutStartDate_ReturnsIEnumerableOfCompanyNewsArticles()
+        {
+            string mockResponse = "{\"totalArticles\":2,\"articles\":[{\"title\":\"Warren Buffett praises AAPL stock in annual letter\",\"description\":\"Warren Buffetts Berkshire Hathaway published its annual letter this morning, and it includes details on its AAPL holdings.\",\"content\":\"Warren Buffetts Berkshire Hathaway Inc. published its highly-anticipated annual letter this morning, offer details on the conglomerates investments in 2020. In this years letter, Buffett touted that Apple ranks as Berkshires biggest common stock ... [2562 chars]\",\"url\":\"https:  9to5mac.com 2021 02 27 warren-buffett-aapl-stake-value \",\"image\":\"https:  i2.wp.com 9to5mac.com wp-content uploads sites 6 2018 08 Warren-Buffett.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1\",\"publishedAt\":\"2021-02-27T19:19:39Z\",\"source\":{\"name\":\"9to5Mac\",\"url\":\"https:  9to5mac.com\"}},{\"title\":\"6 Key Questions Apple Answered At The Annual Shareholder Event\",\"description\":\"Shares of Apple Inc (NASDAQ:AAPL) has taken a beating in the recent market sell-off, spearheaded by tech stocks. At its annual shareholder event T...\",\"content\":\"Shares of Apple Inc (NASDAQ:AAPL) has taken a beating in the recent market sell-off, spearheaded by tech stocks. At its annual shareholder event Tuesday, the company assuaged concerns by answering a slew of questions in the Q&A session.Here are the ... [2278 chars]\",\"url\":\"https:  markets.businessinsider.com news stocks 6-key-questions-apple-answered-at-the-annual-shareholder-event-1030120836\",\"image\":\"https:  cdn.benzinga.com files imagecache 1024x768xUP images story 2012 appletv_timcook_2_2_12.png\",\"publishedAt\":\"2021-02-25T07:07:56Z\",\"source\":{\"name\":\"Business Insider\",\"url\":\"https:  markets.businessinsider.com\"}}]}";
+            _mockMessageHandler = new MockMessageHandler(mockResponse, HttpStatusCode.OK);
+            _httpClient = new HttpClient(_mockMessageHandler);
+            _gNewsRepository = new GNewsRepository(_httpClient);
+            var result = await _gNewsRepository.GetGNewsAsync("AAPL", "", "2021-01-02");
+            result.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public async Task GetNewsAsync_ValidSymbolWithOutEndDate_ReturnsIEnumerableOfCompanyNewsArticles()
+        {
+            string mockResponse = "{\"totalArticles\":2,\"articles\":[{\"title\":\"Warren Buffett praises AAPL stock in annual letter\",\"description\":\"Warren Buffetts Berkshire Hathaway published its annual letter this morning, and it includes details on its AAPL holdings.\",\"content\":\"Warren Buffetts Berkshire Hathaway Inc. published its highly-anticipated annual letter this morning, offer details on the conglomerates investments in 2020. In this years letter, Buffett touted that Apple ranks as Berkshires biggest common stock ... [2562 chars]\",\"url\":\"https:  9to5mac.com 2021 02 27 warren-buffett-aapl-stake-value \",\"image\":\"https:  i2.wp.com 9to5mac.com wp-content uploads sites 6 2018 08 Warren-Buffett.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1\",\"publishedAt\":\"2021-02-27T19:19:39Z\",\"source\":{\"name\":\"9to5Mac\",\"url\":\"https:  9to5mac.com\"}},{\"title\":\"6 Key Questions Apple Answered At The Annual Shareholder Event\",\"description\":\"Shares of Apple Inc (NASDAQ:AAPL) has taken a beating in the recent market sell-off, spearheaded by tech stocks. At its annual shareholder event T...\",\"content\":\"Shares of Apple Inc (NASDAQ:AAPL) has taken a beating in the recent market sell-off, spearheaded by tech stocks. At its annual shareholder event Tuesday, the company assuaged concerns by answering a slew of questions in the Q&A session.Here are the ... [2278 chars]\",\"url\":\"https:  markets.businessinsider.com news stocks 6-key-questions-apple-answered-at-the-annual-shareholder-event-1030120836\",\"image\":\"https:  cdn.benzinga.com files imagecache 1024x768xUP images story 2012 appletv_timcook_2_2_12.png\",\"publishedAt\":\"2021-02-25T07:07:56Z\",\"source\":{\"name\":\"Business Insider\",\"url\":\"https:  markets.businessinsider.com\"}}]}";
+            _mockMessageHandler = new MockMessageHandler(mockResponse, HttpStatusCode.OK);
+            _httpClient = new HttpClient(_mockMessageHandler);
+            _gNewsRepository = new GNewsRepository(_httpClient);
+            var result = await _gNewsRepository.GetGNewsAsync("AAPL", "2021-01-02", "");
             result.Should().NotBeEmpty();
         }
     }
--- StockWebAPI/Repository/GNewsRepository.cs
+++ StockWebAPI/Repository/GNewsRepository.cs
@@ -12,8 +12,10 @@ namespace StockWebAPI.Repository
 {
     public class GNewsRepository
     {
-        private HttpClient _httpClient;
         private const string token = "b9f2a63c4a6767e1f59d49d20b16319e";
+        private readonly string defaultEndDate = DateTime.Now.ToString("yyyy-MM-dd");
+        private readonly string defaultStartDate = DateTime.Now.AddDays(-7).ToString("yyyy-MM-dd");
+        private readonly HttpClient _httpClient;
 
         public GNewsRepository(HttpClient httpClient)
         {
@@ -22,19 +24,39 @@ namespace StockWebAPI.Repository
 
         public async Task<IEnumerable<GNewsCompanyNews.Article>> GetGNewsAsync(string symbol)
         {
-            GNewsCompanyNews.GNewsModel newsModel;
             string apiEndpoint = "search";
-            Uri requestUri = RequestUriHelper(apiEndpoint, symbol);
-            newsModel = await _httpClient.GetFromJsonAsync<GNewsCompanyNews.GNewsModel>(requestUri);
-            return newsModel.articles;
+            Uri requestUri = RequestUriHelper(apiEndpoint, symbol, defaultStartDate, defaultEndDate);
+            try
+            {
+                GNewsCompanyNews.GNewsModel newsModel = await _httpClient.GetFromJsonAsync<GNewsCompanyNews.GNewsModel>(requestUri);
+                return newsModel.articles;
+            }
+            catch (Exception ex)
+            {
+                throw new ApiException($"There was an issue retrieving company information due to: {ex.Message}", ex);
+            }
         }
 
-        private static Uri RequestUriHelper(string apiEndpoint, string symbol)
+        public async Task<IEnumerable<GNewsCompanyNews.Article>> GetGNewsAsync(string symbol, string startDate, string endDate)
+        {
+            string apiEndpoint = "search";
+            Uri requestUri = RequestUriHelper(apiEndpoint, symbol, startDate, endDate);
+            try
+            {
+                GNewsCompanyNews.GNewsModel newsModel = await _httpClient.GetFromJsonAsync<GNewsCompanyNews.GNewsModel>(requestUri);
+                return newsModel.articles;
+            }
+            catch(Exception ex)
+            {
+                throw new ApiException($"There was an issue retrieving company information due to: {ex.Message}", ex);
+            }
+        }
+
+        private static Uri RequestUriHelper(string apiEndpoint, string symbol, string startDate, string endDate)
         {
             string apiBaseKey = "GNews";
             string baseUrl = apiBaseKey.GetBaseUrl();
-            Uri uri = new Uri($"{baseUrl}{apiEndpoint}?q={symbol}&token={token}&lang=en&country=us");
-            return uri;
+            return new Uri($"{baseUrl}{apiEndpoint}?q={symbol}&token={token}&lang=en&country=us&from={startDate}T00:00:00Z&to={endDate}T23:59:00Z");
         }
     }
 }
